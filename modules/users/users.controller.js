4847af51b9931410a50e047e7ba700105f3c23cf
Andre Pinheiro <1247920+mailmannn@users.noreply.github.com>
2018-11-19 11:36:51 +0000

    #AP# more improvments

--- modules/users/users.controller.js
+++ modules/users/users.controller.js
@@ -17,25 +17,25 @@ function register(req, res, next) {
     }
     userService.create(req.body)
         .then(() => res.json({}))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 function getById(req, res, next) {
     userService.getById(req.params.id)
         .then((user) => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 function getAll(req, res, next) {
     userService.getAll()
         .then((users) => res.json(users))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 function getCurrent(req, res, next) {
     userService.getById(req.params.sub)
         .then((user) => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 function update(req, res, next) {
@@ -45,19 +45,19 @@ function update(req, res, next) {
     }
     userService.update(req.params.id, req.body)
         .then(() => res.json({}))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 function _delete(req, res, next) {
     userService.delete(req.params.id)
         .then(() => res.json({}))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 function _deleteByUsername(req, res, next) {
     userService.deleteByUsername(req.params.username)
         .then(() => res.json({}))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 // routes
